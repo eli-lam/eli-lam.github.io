de26f275bcb878888d94c9da61a24d8050009a16
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2019-05-17 00:48:23 -0400

    Add canvas

--- js/eastereggs.js
+++ js/eastereggs.js
@@ -1,80 +1,80 @@
 /* */
 
 function konamiCode(cb) {
-  var input = '';
-  var key = '3838404037393739666513';
-  document.addEventListener('keydown', function (e) {
-    input += ("" + e.keyCode);
+  var input = "";
+  var key = "3838404037393739666513";
+  document.addEventListener("keydown", function(e) {
+    input += "" + e.keyCode;
     if (input === key) {
       return cb();
     }
     if (!key.indexOf(input)) return;
-    input = ("" + e.keyCode);
+    input = "" + e.keyCode;
   });
 }
 
-konamiCode(function () {
-    document.body.style.backgroundImage = "url('./assets/gdimpbg.jpg')";
-    document.getElementById("container").style.display = "inline";
-    document.getElementById("canvas").style.display = "inline-block";
-    document.getElementById("canvas").width = window.innerWidth;
-    document.getElementById("canvas").height = window.innerHeight;
-    draw();
+konamiCode(function() {
+  document.body.style.backgroundImage = "url('./assets/gdimpbg.jpg')";
+  setCanvas();
+  draw();
+});
 
-})
-
-function showFrog() {
-    document.getElementById("frog").style.display = "inline";
+/* Activate canvas */
+function setCanvas() {
+  document.getElementById("container").style.display = "inline";
+  document.getElementById("canvas").style.display = "inline-block";
+  document.getElementById("canvas").width = window.innerWidth;
+  document.getElementById("canvas").height = window.innerHeight;
 }
 
 /* Bouncing frog */
 var ctx = null;
-var x_icon = 0;
-var y_icon = 0;
-var stepX = 3;
-var stepY = 3;
+var anim_img = null;
+
+var x = Math.floor(Math.random() * 100 + 1);
+var y = Math.floor(Math.random() * 100 + 1);
+var xspeed = 3;
+var yspeed = 3;
 var size_x = frog.width;
 var size_y = frog.height;
+
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
-var anim_img = null;
-      
+
+window.addEventListener("resize", function() {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+});
+
 function draw() {
-        var canvas = document.getElementById("canvas");
-        ctx = canvas.getContext("2d");
-        anim_img = new Image(size_x, size_y);
-        anim_img.onload = function() { setInterval('myAnimation()', 10); }
-        anim_img.src = 'https://i.imgur.com/Wnqnl0O.jpg';
+  var canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  anim_img = new Image(size_x, size_y);
+  anim_img.onload = function() {
+    setInterval("bounce()", 8);
+  };
+  anim_img.src = "./assets/frog.jpg";
 }
 
-function myAnimation() {
+function bounce() {
   ctx.clearRect(0, 0, windowWidth, windowHeight);
-       if (x_icon < 0 || x_icon > windowWidth - size_x) {stepX = -stepX; }
-       if (y_icon < 0 || y_icon > windowHeight - size_y) {stepY = -stepY; }
-          x_icon += stepX;
-          y_icon += stepY;
-       ctx.drawImage(anim_img, x_icon, y_icon);
-}
-
+  ctx.drawImage(anim_img, x, y);
   x = x + xspeed;
   y = y + yspeed;
 
-  if (x + dvd.width >= width) {
+  if (x + size_x >= windowWidth) {
     xspeed = -xspeed;
-    x = width - dvd.width;
-    pickColor();
+    x = windowWidth - size_x;
   } else if (x <= 0) {
     xspeed = -xspeed;
     x = 0;
-    pickColor();
   }
 
-  if (y + dvd.height >= height) {
+  if (y + size_y >= windowHeight) {
     yspeed = -yspeed;
-    y = height - dvd.height;
-    pickColor();
+    y = windowHeight - size_y;
   } else if (y <= 0) {
     yspeed = -yspeed;
     y = 0;
-    pickColor();
   }
+}
